1403e145fc5937c3e27c12d60e4aee85bc598065
kakaism <91718883+kakaism@users.noreply.github.com>
2021-10-02 11:56:07 +0530

    :fix: skip deepsource PYL-W0221

--- users/models.py
+++ users/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
-# Create your models here.
-
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -12,7 +10,7 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs):  # skipcq PYL-W0221
         super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
