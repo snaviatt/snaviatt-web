f1f2d9bc14dd2c5cf281a8695624330c7ae82f94
kakaism <91718883+kakaism@users.noreply.github.com>
2021-10-02 11:47:00 +0530

    :bug: Fix Deepsource issue PYL-W0613

--- users/views.py
+++ users/views.py
@@ -40,7 +40,7 @@ def profile(request):
     return render(request, 'users/profile.html', context)
 
 
-def handler404(request, exception, template_name="facematch/404.html"):
-    response = render_to_response("404.html")
+def handler404(request, _, template_name="facematch/404.html"):
+    response = render_to_response(template_name)
     response.status_code = 404
     return response
